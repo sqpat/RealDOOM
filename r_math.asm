aec1f4e4b3fb341e1c70a14fd0a179e697fcf67a
sqpat <sqpat17@gmail.com>
2024-07-24 09:04:17 -0700

    r_pointtoangle small branch, jump optimizations

--- r_math.asm
+++ r_math.asm
@@ -26,131 +26,29 @@ INCLUDE defs.inc
 
 .CODE
 
-;R_PointToAngle_
 
-PROC R_PointToAngle_
-PUBLIC R_PointToAngle_
-
-; inputs:
-; DX:AX = x  (32 bit fixed pt 16:16)
-; CX:BX = y  (32 bit fixed pt 16:16)
-
-; places to improve -
-; 1.default branches taken. count branches taken and modify to optimize
-; 2.double jump on jne (out of range of relative)
-
-;	x.w -= viewx.w;
-;	y.w -= viewy.w;
-
-
-
-sub   ax, word ptr [_viewx]
-sbb   dx, word ptr [_viewx+2]
-
-sub   bx, word ptr [_viewy]
-sbb   cx, word ptr [_viewy+2]
-
-; 	if ((!x.w) && (!y.w))
-;		return 0;
-
-test  dx, dx
-jne   inputs_not_zero   ; todo rearrange this. rare case
-test  cx, cx
-jne   inputs_not_zero   ; todo rearrange this. rare case
-test  ax, ax
-jne   inputs_not_zero   ; todo rearrange this. rare case
-test  bx, bx
-jne   inputs_not_zero   ; todo rearrange this. rare case
-return_0:
-
-xor   ax, ax
-cwd
-
-retf  
-
-
-inputs_not_zero:
-
-test  dx, dx
-jg    x_is_positive
-jne   x_is_negative_1
-
-x_is_positive:
+octant_6:
 test  cx, cx
 
-jg    y_is_positive
-jne   y_is_negative_1
-y_is_positive:
-
-cmp   dx, cx
-jg    octant_0
-
-jne   octant_1
-cmp   ax, bx
-jbe   octant_1
-
-
-octant_0:
-test  dx, dx
-
-;	if (x.w < 512)
-
-jne   octant_0_do_divide
-cmp   ax, 0200h
-jae   octant_0_do_divide
-octant_0_out_of_bounds:
-mov   dx, 02000h
+jne   octant_6_do_divide
+cmp   bx, 0200h
+jae   octant_6_do_divide
+octant_6_out_of_bounds:
+mov   dx, 0e000h
 xor   ax, ax
 
 retf  
-
-
-octant_0_do_divide:
-;x_is_negative
-xchg dx, cx
-xchg ax, bx
+octant_6_do_divide:
 call FastDiv3232_shift_3_8_
 cmp   ax, 0800h
-jae   octant_0_out_of_bounds
-
+jae   octant_6_out_of_bounds
 mov   bx, word ptr [_tantoangle]
 shl   ax, 2
 mov   es, word ptr [_tantoangle+2]
 add   bx, ax
 mov   ax, word ptr es:[bx]
 mov   dx, word ptr es:[bx + 2]
-
-retf  
-
-x_is_negative_1:
-
-jmp   x_is_negative
-y_is_negative_1:
-jmp   y_is_negative
-
-octant_1:
-test  cx, cx
-
-jne   octant_1_do_divide
-cmp   bx, 0200h
-jae   octant_1_do_divide
-octant_1_out_of_bounds:
-mov   ax, 0ffffh
-mov   dx, 01fffh
-
-retf  
-octant_1_do_divide:
-call FastDiv3232_shift_3_8_
-cmp   ax, 0800h
-jae   octant_1_out_of_bounds
-mov   bx, word ptr [_tantoangle]
-shl   ax, 2
-mov   es, word ptr [_tantoangle+2]
-add   bx, ax
-mov   ax, 0ffffh
-sub   ax, word ptr es:[bx]
-mov   dx, 03fffh
-sbb   dx, word ptr es:[bx + 2]
+add   dx, 0c000h
 
 retf  
 
@@ -209,30 +107,128 @@ neg   ax
 sbb   dx, 0
 
 retf  
-octant_6:
+
+;R_PointToAngle_
+
+PROC R_PointToAngle_
+PUBLIC R_PointToAngle_
+
+; inputs:
+; DX:AX = x  (32 bit fixed pt 16:16)
+; CX:BX = y  (32 bit fixed pt 16:16)
+
+; places to improve -
+; 1.default branches taken. count branches taken and modify to optimize
+
+;	x.w -= viewx.w;
+;	y.w -= viewy.w;
+
+
+
+sub   ax, word ptr [_viewx]
+sbb   dx, word ptr [_viewx+2]
+
+sub   bx, word ptr [_viewy]
+sbb   cx, word ptr [_viewy+2]
+
+; 	if ((!x.w) && (!y.w))
+;		return 0;
+
+test  dx, dx
+jne   inputs_not_zero   ; todo rearrange this. rare case
+test  cx, cx
+jne   inputs_not_zero   ; todo rearrange this. rare case
+test  ax, ax
+jne   inputs_not_zero   ; todo rearrange this. rare case
+test  bx, bx
+jne   inputs_not_zero   ; todo rearrange this. rare case
+return_0:
+
+xor   ax, ax
+cwd
+
+retf  
+
+
+inputs_not_zero:
+
+test  dx, dx
+jl   x_is_negative
+
+x_is_positive:
 test  cx, cx
 
-jne   octant_6_do_divide
-cmp   bx, 0200h
-jae   octant_6_do_divide
-octant_6_out_of_bounds:
-mov   dx, 0e000h
+jl   y_is_negative
+y_is_positive:
+
+cmp   dx, cx
+jg    octant_0
+
+jne   octant_1
+cmp   ax, bx
+jbe   octant_1
+
+
+octant_0:
+test  dx, dx
+
+;	if (x.w < 512)
+
+jne   octant_0_do_divide
+cmp   ax, 0200h
+jae   octant_0_do_divide
+octant_0_out_of_bounds:
+mov   dx, 02000h
 xor   ax, ax
 
 retf  
-octant_6_do_divide:
+
+
+octant_0_do_divide:
+;x_is_negative
+xchg dx, cx
+xchg ax, bx
 call FastDiv3232_shift_3_8_
 cmp   ax, 0800h
-jae   octant_6_out_of_bounds
+jae   octant_0_out_of_bounds
+
 mov   bx, word ptr [_tantoangle]
 shl   ax, 2
 mov   es, word ptr [_tantoangle+2]
 add   bx, ax
 mov   ax, word ptr es:[bx]
 mov   dx, word ptr es:[bx + 2]
-add   dx, 0c000h
 
 retf  
+
+
+octant_1:
+test  cx, cx
+
+jne   octant_1_do_divide
+cmp   bx, 0200h
+jae   octant_1_do_divide
+octant_1_out_of_bounds:
+mov   ax, 0ffffh
+mov   dx, 01fffh
+
+retf  
+octant_1_do_divide:
+call FastDiv3232_shift_3_8_
+cmp   ax, 0800h
+jae   octant_1_out_of_bounds
+mov   bx, word ptr [_tantoangle]
+shl   ax, 2
+mov   es, word ptr [_tantoangle+2]
+add   bx, ax
+mov   ax, 0ffffh
+sub   ax, word ptr es:[bx]
+mov   dx, 03fffh
+sbb   dx, word ptr es:[bx + 2]
+
+retf  
+
+
 
 x_is_negative:
 
@@ -245,7 +241,7 @@ sbb   dx, 0
 test  cx, cx
 
 jg    y_is_positive_x_neg
-jne   y_is_negative_x_neg_1
+jne   y_is_negative_x_neg
 y_is_positive_x_neg:
 cmp   dx, cx
 jg    octant_3
@@ -289,8 +285,6 @@ octant_2_out_of_bounds:
 mov   dx, 06000h
 xor   ax, ax
 retf  
-y_is_negative_x_neg_1:
-jmp   y_is_negative_x_neg
 octant_2_do_divide:
 
 call FastDiv3232_shift_3_8_
