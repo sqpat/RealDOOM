be9cbb229b34a49c75db7cd1e427aeb3a957450a
sqpat <sqpat17@gmail.com>
2024-08-01 12:27:42 -0700

    drawfuzzcolumn reduce some reg swapping

--- r_masked.asm
+++ r_masked.asm
@@ -468,11 +468,10 @@ cbw
 mov  bx, di
 mov  es, cx
 add  bx, ax
-mov  al, byte ptr es:[bx]
-mov  bx, COLORMAPS_HIGH_SEG_DIFF_SEGMENT
-xor  ah, ah
-mov  es, bx
-mov  bx, ax
+mov  bl, byte ptr es:[bx]
+mov  ax, COLORMAPS_HIGH_SEG_DIFF_SEGMENT
+xor  bh, bh
+mov  es, ax
 mov  al, byte ptr es:[bx]
 mov  es, cx
 inc  dl
