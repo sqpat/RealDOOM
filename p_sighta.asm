bdaab0d7a45dcc04e963c8a4190350ab9d01571f
sqpat <sqpat17@gmail.com>
2025-04-09 13:44:39 +0900

    p_checksight remove stack frame and lots of logic simplication

--- p_sighta.asm
+++ p_sighta.asm
@@ -48,30 +48,31 @@ PUBLIC  P_CheckSight_
 
 push  si
 push  di
-push  bp
-mov   bp, sp
-push cx			; bp - 2	; todo unused
-push ax			; bp - 4
-push dx			; bp - 6
-sub   sp, 08h
+
+push dx			; bp - 2
+
 mov   si, cx    ; si gets t2_pos
 
 ; todo clean up this di shuffling
-xchg  ax, cx
-mov   di, dx
+mov   es, ax    ; es holds t1
+mov   di, dx    ; di gets t2
 mov   ax, word ptr [di + 4]
 cwd   
-mov   di, cx  ; bp - 4 value
+
 xchg  ax, cx  ; back up low
+mov   di, es    ; di gets t1 
 mov   ax, word ptr [di + 4]
-mov   di, dx  ; back up high
+mov   di, dx  ; back up high	di:cx holds first result
 
 mul   word ptr ds:[_numsectors]
 add   ax, cx
 adc   dx, di
 
-mov   cx, ax	; cx is preshifted
+; generate reject bitmap lookup
+mov   cx, ax	; cx is preshifted	
+and   cl, 7		; will shift by this amount for bit count
 
+; divide by 8 for 8 bits per byte..
 shr   dx, 1
 rcr   ax, 1
 shr   dx, 1
@@ -79,18 +80,22 @@ rcr   ax, 1
 shr   dx, 1
 rcr   ax, 1
 
-mov   dx, 1
-xchg  di, ax ; di gets post-shifted.
-mov   ax, REJECTMATRIX_SEGMENT
-mov   es, ax
-and   cl, 7
-mov   al, byte ptr es:[di]
-shl   dx, cl
-xor   ah, ah
-test  ax, dx
+xchg  di, ax 			; di gets post-shifted.
+
+mov   al, 1
+shl   al, cl			; dl stores bit for bit test
+
+mov   cx, es			; store t1 again
+
+mov   dx, REJECTMATRIX_SEGMENT
+mov   es, dx
+
+test  al, byte ptr es:[di]	; bit test the byte
+
 je    not_in_reject_table
-xor   al, al
-LEAVE_MACRO
+xor   ax, ax				; return 0
+
+pop   dx	; clean out the push earlier...
 pop   di
 pop   si
 retf 
@@ -101,50 +106,64 @@ inc   word ptr ds:[_validcount]
 
 mov   ax, MOBJPOSLIST_6800_SEGMENT
 mov   es, ax
+push  si  					; store for now
 
-mov   di, word ptr [bp - 4]
-mov   ax, word ptr es:[bx + 8]
-mov   word ptr [bp - 0Ah], ax
+; todo is lds worth it for these dword reads..
+mov   di, cx				; grab t1 again
 
+;    sightzstart = t1_pos->z.w + t1->height.w - (t1->height.w>>2);
+
+mov   cx, word ptr es:[bx + 8]
+mov   si, word ptr es:[bx + 0Ah]
 mov   ax, word ptr [di + 0Ah]
-mov   dx, word ptr es:[bx + 0Ah]
-add   word ptr [bp - 0Ah], ax
-adc   dx, word ptr [di + 0Ch]
-mov   word ptr [bp - 8], dx
 mov   dx, word ptr [di + 0Ch]
+add   cx, ax
+adc   si, dx			; si:cx result
+
 sar   dx, 1
 rcr   ax, 1
-mov   cx, word ptr [bp - 0Ah]
 sar   dx, 1
 rcr   ax, 1
 sub   cx, ax
-mov   ax, word ptr [bp - 8]
-sbb   ax, dx
+
+sbb   si, dx
 mov   word ptr ds:[_sightzstart], cx
-mov   word ptr ds:[_sightzstart + 2], ax
+mov   word ptr ds:[_sightzstart + 2], si
 
 ; todo: a lot of movsw below
 
-mov   di, word ptr [bp - 6]
+xchg  ax, si				; ax gets sightzstart+2
+pop   si					; recover si
+pop   di 					; grab t2 again
+push  bx					; store this...
+xchg  ax, bx				; bx gets sightzstart+2
+
+;    topslope = (t2_pos->z.w+t2->height.w) - sightzstart;
+
 mov   ax, word ptr es:[si + 8]
 mov   dx, word ptr es:[si + 0Ah]
 add   ax, word ptr [di + 0Ah]
-adc   dx, word ptr [di + 0Ch]
-mov   word ptr [bp - 0Ah], dx
-mov   dx, cx
-mov   cx, word ptr ds:[_sightzstart + 2]
-sub   ax, dx
+adc   dx, word ptr [di + 0Ch]	; last di use...
+
+
+mov   di, bx			; di gets sightzstart+2
+mov   bx, dx			
+mov   dx, cx			; cx had sightz_start
+
+sub   ax, dx			; subtract sightzstart	
+sbb   bx, di			
 mov   word ptr ds:[_topslope], ax
-mov   ax, word ptr [bp - 0Ah]
-sbb   ax, cx
-mov   word ptr ds:[_topslope+2], ax
+mov   word ptr ds:[_topslope+2], bx
+
+
 mov   ax, word ptr es:[si + 8]
 mov   cx, word ptr es:[si + 0Ah]
-sub   ax, dx
-sbb   cx, word ptr ds:[_sightzstart + 2]
+sub   ax, dx			; subtract sightzstart
+sbb   cx, di			
 mov   word ptr ds:[_bottomslope], ax
 mov   word ptr ds:[_bottomslope+2], cx
 
+pop   bx				; restore bx (t1 pos?)
 mov   ax, word ptr es:[bx]
 mov   word ptr ds:[_strace], ax
 mov   ax, word ptr es:[bx + 2]
@@ -183,7 +202,7 @@ mov   ax, word ptr ds:[_numnodes]
 dec   ax
 call  P_CrossBSPNode_
 
-LEAVE_MACRO
+
 pop   di
 pop   si
 retf 
