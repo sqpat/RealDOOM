746d97b12475f433bf8e261348a0d2ec358e843c
sqpat <sqpat17@gmail.com>
2025-07-10 09:31:25 -0700

    missed adding a file

---
+++ p_mobjn.asm
@@ -0,0 +1,143 @@
+; Copyright (C) 1993-1996 Id Software, Inc.
+; Copyright (C) 1993-2008 Raven Software
+; Copyright (C) 2016-2017 Alexey Khokholov (Nuke.YKT)
+;
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License
+; as published by the Free Software Foundation; either version 2
+; of the License, or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; DESCRIPTION:
+;
+INCLUDE CONSTANT.INC
+INCLUDE defs.inc
+INSTRUCTION_SET_MACRO
+
+
+.DATA
+
+
+.CODE
+
+
+; todo move this to p_mobja when its not called anymore from doom.exe code
+; openwatcom wont allow pragma to force z param into di/si when called as a variable instead of function
+
+
+;void __near P_SpawnPuff ( fixed_t	x, fixed_t	y, fixed_t	z ){
+
+;P_SpawnPuff_
+
+PROC P_SpawnPuff_ FAR
+PUBLIC P_SpawnPuff_
+
+
+push  ax
+push  dx
+push  bx
+
+mov   ax, RNDTABLE_SEGMENT
+mov   es, ax
+
+mov   al, byte ptr ds:[_prndindex]
+add   byte ptr ds:[_prndindex], 3  ; for 3 calls this func..
+xor   ah, ah
+mov   bx, ax
+inc   bx
+mov   al, byte ptr es:[bx]
+sub   al, byte ptr es:[bx+1]
+
+sbb   ah, 0
+cwd
+
+; shift ax left 10
+mov   dl, ah ; shift 8
+mov   ah, al ; shift 8
+sal   ax, 1
+rcl   dx, 1
+sal   ax, 1
+rcl   dx, 1
+and   ax, 0FC00h  ; clean out bottom bits
+
+
+add   si, ax
+adc   di, dx
+
+mov   al, byte ptr es:[bx+2]
+mov   byte ptr cs:[SELFMODIFY_set_rnd_value_3+1], al  
+
+pop   bx
+pop   dx
+pop   ax
+
+IF COMPISA GE COMPILE_186
+
+push  -1        ; complicated for 8088...
+push  MT_PUFF
+push  di
+push  si
+
+
+ELSE
+
+mov   es, si
+mov   si, -1
+push  si
+mov   si, MT_PUFF
+push  si
+push  di
+push  es
+
+
+ENDIF
+
+
+;call  P_SpawnMobj_
+db 0FFh  ; lcall[addr]
+db 01Eh  ;
+dw _P_SpawnMobj_addr
+;	 th = setStateReturn;
+;    th->momz.h.intbits = 1;
+;    th->tics -= P_Random()&3;
+
+mov   bx, word ptr ds:[_setStateReturn];
+mov   word ptr [bx + 018h], 1
+SELFMODIFY_set_rnd_value_3:
+mov   al, 0FFh
+and   al, 3
+sub   byte ptr [bx + 01Bh], al
+
+;    if (th->tics < 1 || th->tics > 240){
+;		th->tics = 1;
+;	}
+
+
+mov   al, byte ptr [bx + 01Bh]
+cmp   al, 1
+jb    set_tics_to_1
+cmp   al, 240
+jbe   dont_set_tics_to_1
+set_tics_to_1:
+mov   byte ptr [bx + 01Bh], 1
+dont_set_tics_to_1:
+cmp   word ptr ds:[_attackrange16], MELEERANGE
+je    spark_punch_on_wall
+retf  
+spark_punch_on_wall:
+mov   dx, S_PUFF3
+mov   ax, bx
+;call  P_SetMobjState_
+db 0FFh  ; lcall[addr]
+db 01Eh  ;
+dw _P_SetMobjState_addr
+retf   
+
+
+
+ENDP
+END
