5f4bf1e8681cf1e976c5459f059b58661d89a954
sqpat <sqpat17@gmail.com>
2025-07-12 09:18:26 -0700

    p_enemya.asm and build files

---
+++ p_enemya.asm
@@ -0,0 +1,49 @@
+; Copyright (C) 1993-1996 Id Software, Inc.
+; Copyright (C) 1993-2008 Raven Software
+; Copyright (C) 2016-2017 Alexey Khokholov (Nuke.YKT)
+;
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License
+; as published by the Free Software Foundation; either version 2
+; of the License, or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; DESCRIPTION:
+;
+INCLUDE CONSTANT.INC
+INCLUDE defs.inc
+INSTRUCTION_SET_MACRO
+
+
+; hack but oh well
+P_SIGHT_STARTMARKER_ = 0 
+
+
+
+.DATA
+
+
+.CODE
+
+
+
+PROC    P_ENEMY_STARTMARKER_ 
+PUBLIC  P_ENEMY_STARTMARKER_
+ENDP
+
+
+
+
+
+
+PROC    P_ENEMY_ENDMARKER_ 
+PUBLIC  P_ENEMY_ENDMARKER_
+ENDP
+
+
+
+END
