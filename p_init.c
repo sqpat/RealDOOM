05552bffb2a9f28a97e1cd75563dff5d2902ff35
sqpat <sqpat17@gmail.com>
2025-12-26 13:01:20 -0500

    R_InitSpriteDefs_ asm first pass

--- p_init.c
+++ p_init.c
@@ -341,6 +341,28 @@ void __near R_InstallSpriteLump (int16_t lump, uint16_t frame, uint16_t rotation
 //
 
 
+/*
+
+int8_t *namelist[NUMSPRITES] = {
+	"TROO","SHTG","PUNG","PISG","PISF","SHTF","SHT2","CHGG","CHGF","MISG",
+	"MISF","SAWG","PLSG","PLSF","BFGG","BFGF","BLUD","PUFF","BAL1","BAL2",
+	"PLSS","PLSE","MISL","BFS1","BFE1","BFE2","TFOG","IFOG","PLAY","POSS",
+	"SPOS","VILE","FIRE","FATB","FBXP","SKEL","MANF","FATT","CPOS","SARG",
+	"HEAD","BAL7","BOSS","BOS2","SKUL","SPID","BSPI","APLS","APBX","CYBR",
+	"PAIN","SSWV","KEEN","BBRN","BOSF","ARM1","ARM2","BAR1","BEXP","FCAN",
+	"BON1","BON2","BKEY","RKEY","YKEY","BSKU","RSKU","YSKU","STIM","MEDI",
+	"SOUL","PINV","PSTR","PINS","MEGA","SUIT","PMAP","PVIS","CLIP","AMMO",
+	"ROCK","BROK","CELL","CELP","SHEL","SBOX","BPAK","BFUG","MGUN","CSAW",
+	"LAUN","PLAS","SHOT","SGN2","COLU","SMT2","GOR1","POL2","POL5","POL4",
+	"POL3","POL1","POL6","GOR2","GOR3","GOR4","GOR5","SMIT","COL1","COL2",
+	"COL3","COL4","CAND","CBRA","COL6","TRE1","TRE2","ELEC","CEYE","FSKU",
+	"COL5","TBLU","TGRN","TRED","SMBT","SMGT","SMRT","HDB1","HDB2","HDB3",
+	"HDB4","HDB5","HDB6","POB1","POB2","BRS1","TLMP","TLP2"
+};
+*/
+
+void __near R_InitSpriteDefs() ;
+/*
 void __near R_InitSpriteDefs() {
 	int16_t         i;
 	int16_t         l;
@@ -358,32 +380,13 @@ void __near R_InitSpriteDefs() {
 	int8_t localname[8];
 	lumpinfo_t __far* lumpinfoInit = ((lumpinfo_t __far*) MK_FP(WAD_PAGE_FRAME_PTR, 0));
 
-	/*
-
-	int8_t *namelist[NUMSPRITES] = {
-		"TROO","SHTG","PUNG","PISG","PISF","SHTF","SHT2","CHGG","CHGF","MISG",
-		"MISF","SAWG","PLSG","PLSF","BFGG","BFGF","BLUD","PUFF","BAL1","BAL2",
-		"PLSS","PLSE","MISL","BFS1","BFE1","BFE2","TFOG","IFOG","PLAY","POSS",
-		"SPOS","VILE","FIRE","FATB","FBXP","SKEL","MANF","FATT","CPOS","SARG",
-		"HEAD","BAL7","BOSS","BOS2","SKUL","SPID","BSPI","APLS","APBX","CYBR",
-		"PAIN","SSWV","KEEN","BBRN","BOSF","ARM1","ARM2","BAR1","BEXP","FCAN",
-		"BON1","BON2","BKEY","RKEY","YKEY","BSKU","RSKU","YSKU","STIM","MEDI",
-		"SOUL","PINV","PSTR","PINS","MEGA","SUIT","PMAP","PVIS","CLIP","AMMO",
-		"ROCK","BROK","CELL","CELP","SHEL","SBOX","BPAK","BFUG","MGUN","CSAW",
-		"LAUN","PLAS","SHOT","SGN2","COLU","SMT2","GOR1","POL2","POL5","POL4",
-		"POL3","POL1","POL6","GOR2","GOR3","GOR4","GOR5","SMIT","COL1","COL2",
-		"COL3","COL4","CAND","CBRA","COL6","TRE1","TRE2","ELEC","CEYE","FSKU",
-		"COL5","TBLU","TGRN","TRED","SMBT","SMGT","SMRT","HDB1","HDB2","HDB3",
-		"HDB4","HDB5","HDB6","POB1","POB2","BRS1","TLMP","TLP2"
-	};
-	*/
 	int8_t namelist[NUMSPRITES][5];
 	FILE * fp = fopen("DOOMDATA.BIN", "rb"); // clear old file
 	fseek(fp, SPLIST_DOOMDATA_OFFSET, SEEK_SET);
 	fread(namelist,  5, NUMSPRITES, fp);
 	fclose(fp);
+	
 	//I_Error("\nvalanim %s %s %s", namelist[0], namelist[1], namelist[2]);
-
 	// count the number of sprite names
 
 
@@ -395,7 +398,7 @@ void __near R_InitSpriteDefs() {
 	//if (!numsprites)
 	//	return;
 
-	
+	// SPRITES:SEGMENT:0193
 	currentspritememoryoffset = sprites[0].spriteframesOffset = NUMSPRITES * sizeof(spritedef_t);
 
 	//totalsize = numsprites * sizeof(spritedef_t);
@@ -496,7 +499,7 @@ void __near R_InitSpriteDefs() {
 	}
 	//I_Error("\n%u %x", currentspritememoryoffset, currentspritememoryoffset)
 }
-
+*/
 
 
 //
--- p_inita.asm
+++ p_inita.asm
@@ -18,14 +18,16 @@
 INCLUDE defs.inc
 INSTRUCTION_SET_MACRO
 
-EXTRN  Z_QuickMapPhysics_:FAR
-EXTRN  Z_QuickMapRender_:FAR
-EXTRN  Z_QuickMapRenderPlanes_:FAR
-EXTRN  Z_QuickMapUndoFlatCache_:FAR
-EXTRN  FixedDivWholeA_:FAR
-EXTRN  FixedMul_:FAR
-EXTRN  FixedMul_:FAR
-EXTRN  CopyString13_:NEAR
+EXTRN Z_QuickMapPhysics_:FAR
+EXTRN Z_QuickMapRender_:FAR
+EXTRN Z_QuickMapRenderPlanes_:FAR
+EXTRN Z_QuickMapUndoFlatCache_:FAR
+EXTRN Z_QuickMapWADPageFrame_:FAR
+EXTRN copystr8_:NEAR
+EXTRN FixedDivWholeA_:FAR
+EXTRN FixedMul_:FAR
+EXTRN FixedMul_:FAR
+EXTRN CopyString13_:NEAR
 EXTRN fread_:FAR
 EXTRN fseek_:FAR
 EXTRN fopen_:FAR
@@ -36,11 +38,14 @@ EXTRN R_TextureNumForName_:NEAR
 EXTRN R_CheckTextureNumForName_:NEAR
 EXTRN R_FlatNumForName_:NEAR
 EXTRN W_CheckNumForName_:NEAR
+EXTRN R_InstallSpriteLump_:NEAR
+EXTRN W_GetNumForName_:FAR
+
  
 .DATA
 
-
-
+EXTRN _p_init_maxframe:WORD
+EXTRN _p_init_sprtemp:DWORD
 .CODE
 
 EXTRN  _doomdata_bin_string:NEAR
@@ -53,6 +58,8 @@ ENDP
 NUMSWITCHDEFS = 41
 BAD_TEXTURE = 0FFFFh
 NUMANIMDEFS = 23
+NUMSSPRITEDEFS = 29
+
 PROC    P_InitSwitchList_ NEAR
 PUBLIC  P_InitSwitchList_
 
@@ -266,6 +273,269 @@ loop  loop_next_animdef
 
 mov   word ptr ds:[_lastanim], di
 
+LEAVE_MACRO
+POPA_NO_AX_OR_BP_MACRO
+ret
+
+ENDP
+
+SIZE_SPRITE_NAMES = 5 * NUMSPRITES
+SIZE_LOCALNAME =    (5 * NUMSPRITES) + 8
+; plus 1 because odd..
+SIZE_SPR_TEMP = (NUMSSPRITEDEFS * (SIZE SPRITEFRAME_T)) + (5 * NUMSPRITES) + 8 + 1
+
+; 
+
+
+PROC    R_InitSpriteDefs_ NEAR
+PUBLIC  R_InitSpriteDefs_
+
+PUSHA_NO_AX_OR_BP_MACRO
+push   bp
+mov    bp, sp
+; 690 + 725 + 8 + 1 = 1424
+sub    sp, SIZE_SPR_TEMP
+
+mov   ax, OFFSET _doomdata_bin_string
+call  CopyString13_
+mov   dx, OFFSET  _fopen_rb_argument
+call  fopen_        ; fopen("DOOMDATA.BIN", "rb"); 
+mov   di, ax ; di stores fp
+
+xor   dx, dx  ; SEEK_SET
+mov   bx, SPLIST_DOOMDATA_OFFSET AND 0FFFFh
+mov   cx, SPLIST_DOOMDATA_OFFSET SHR 16
+call  fseek_  ;    fseek(fp, SWITCH_DOOMDATA_OFFSET, SEEK_SET);
+
+;fread(namelist,  5, NUMSPRITES, fp);
+lea   ax, [bp - SIZE_SPRITE_NAMES]
+mov   cx, di ; fp
+mov   dx, 5
+mov   bx, NUMSPRITES
+call  fread_
+
+xchg  ax, di
+call  fclose_
+
+
+;	p_init_sprtemp = (spriteframe_t __far *) &sprtempbytes;
+mov   word ptr ds:[_p_init_sprtemp+0], sp   ; equal to [bp - SIZE_SPR_TEMP]
+mov   word ptr ds:[_p_init_sprtemp+2], ss
+;	lumpinfo_t __far* lumpinfoInit = ((lumpinfo_t __far*) MK_FP(WAD_PAGE_FRAME_PTR, 0));
+; localname is bp - SIZE_SPRITE_NAMES - 8
+
+
+
+; loop start
+
+mov   ax, word ptr ds:[_firstspritelump]	
+mov   word ptr cs:[_SELFMODIFY_setstart + 1], ax
+add   ax, word ptr ds:[_numspritelumps]
+mov   word ptr cs:[_SELFMODIFY_set_end + 2], ax
+
+lea   si, [bp - SIZE_SPRITE_NAMES]  ; namelist
+
+loop_next_sprite:
+les   di, dword ptr ds:[_p_init_sprtemp]
+;		FAR_memset(p_init_sprtemp, -1, sizeof(p_init_sprtemp));
+mov   ax, -1
+mov   cx, ((NUMSSPRITEDEFS * (SIZE SPRITEFRAME_T)) / 2) + 1  ; round up... we added an extra byte anyway.
+rep   stosw
+mov   word ptr ds:[_p_init_maxframe], ax
+
+lodsw
+xchg  ax, dx
+lodsw
+xchg  ax, cx
+; dx then cx are the 4 characters
+inc   si  ; 5th byte of the name is 0, ignore
+
+push  si  ; store this loop var.
+
+_SELFMODIFY_setstart:
+mov   si, 01000h
+
+;		for (l = start + 1; l < end; l++) {
+
+mov   di, si ; di will be the lump offset
+; sizeof lumpinfo_t is 16
+SHIFT_MACRO shl di 4
+
+loop_start_to_end:
+
+;    int16_t lump = l & (LUMP_PER_EMS_PAGE - 1);
+mov   ax, si
+
+call  Z_QuickMapWADPageFrame_
+mov   es, word ptr ds:[_WAD_PAGE_FRAME_PTR]
+and   di, 16383 
+
+
+;     if (*(int32_t  __far*)lumpinfoInit[lump].name == intname) {
+; compare four characters basically
+cmp   dx, word ptr es:[di + 0]
+jne   not_this_sprite
+cmp   cx, word ptr es:[di + 2]
+je    is_this_sprite
+not_this_sprite:
+done_processing_sprite_lump:
+
+add   di, SIZE LUMPINFO_T
+inc   si 
+_SELFMODIFY_set_end:
+cmp   si, 01000h
+jb    loop_start_to_end
+
+pop   si  ; recover name list index
+
+;    // check the frames that were found for completeness
+;    if (p_init_maxframe == -1) {
+;        sprites[i].numframes = 0;
+;        continue;
+;    }
+
+mov   ax, SPRITES_SEGMENT
+mov   es, ax
+
+;  si = [bp - SIZE_SPRITE_NAMES] + i * 5
+;  so ax should be ((si - bp + SIZE_SPRITE_NAMES) / 5) to get i
+
+lea   ax, [si + SIZE_SPRITE_NAMES - 5] ; minus 5 because we pre-advanced si via lodsw/lodsb
+sub   ax, bp
+mov   bl, 5
+div   bl
+;    ax is sprite index. sprite offset is * 3 (size spriteframe_t)
+mov   bx, ax
+shl   bx, 1
+add   bx, ax
+
+
+cmp   byte ptr ds:[_p_init_maxframe], ah ; known zero
+jl    set_no_frames  ; less than 0 means -1 in this case
+
+; p_init_maxframe++
+inc   word ptr ds:[_p_init_maxframe]
+
+; NOTE this whole loop only error checks. Remove?
+
+COMMENT @
+mov   cx, word ptr ds:[_p_init_maxframe]
+
+;    for (frame = 0; frame < p_init_maxframe; frame++) {
+lea   bx, [bp - SIZE_SPR_TEMP]
+loop_spriteframe_rotation_check:
+mov   al, byte ptr ds:[bx + SPRITEFRAME_T.spriteframe_rotate]
+
+add    bx, SIZE SPRITEFRAME_T
+loop   loop_spriteframe_rotation_check
+@
+
+;	sprites[i].numframes = p_init_maxframe; // this  isnever used outside of here and r_setup...
+;	sprites[i].spriteframesOffset = currentspritememoryoffset;
+;	currentspritememoryoffset += (p_init_maxframe * sizeof(spriteframe_t));
+
+
+mov   al, byte ptr ds:[_p_init_maxframe]
+mov   byte ptr es:[bx + SPRITEDEF_T.spritedef_numframes], al
+mov   ah, SIZE SPRITEFRAME_T
+mul   ah
+xchg  ax, cx
+
+_SELFMODIFY_set_new_currentspritememoryoffset:
+mov   di, NUMSPRITES * SIZE SPRITEDEF_T ; default starting point for the data.
+mov   word ptr es:[bx + SPRITEDEF_T.spritedef_spriteframesOffset], di
+
+;	spriteframes = (spriteframe_t __far*)&(spritedefs_bytes[sprites[i].spriteframesOffset]);
+;	FAR_memcpy(spriteframes, p_init_sprtemp, p_init_maxframe * sizeof(spriteframe_t));
+
+; es is already sprites segment. starting data for the sprite frame data comes after the spriteframe
+; cx is already len
+; di is already dest
+; set si to source
+
+lea   bx, [bp - SIZE_SPR_TEMP]
+xchg  bx, si
+shr   cx, 1
+rep   movsw
+adc   cx, cx
+rep   movsb
+xchg  bx, si
+
+mov   word ptr cs:[_SELFMODIFY_set_new_currentspritememoryoffset + 1], di  ; new position for next data copy.
+
+continue_inner_loop:
+
+cmp   si, bp     ; bp is the end of this string list
+jnb   exit_initspritedefs
+jmp   loop_next_sprite
+
+
+set_no_frames:
+;			sprites[i].numframes = 0;
+mov   byte ptr es:[bx + SPRITEDEF_T.spritedef_numframes], ah  ; known zero
+jmp   continue_inner_loop; 
+
+is_this_sprite:
+
+; store name stuff
+push  cx
+push  dx  
+
+;    if (modifiedgame) {
+cmp   byte ptr ds:[_modifiedgame], 0
+je    not_modified
+is_modified:
+;    copystr8(localname, lumpinfoInit[lump].name);
+;    patched = W_GetNumForName(localname);
+
+lea   ax, [bp - SIZE_LOCALNAME]
+mov   dx, ss
+mov   cx, es
+mov   bx, di
+call  copystr8_
+
+lea   ax, [bp - SIZE_LOCALNAME]
+mov   dx, ss
+call  W_GetNumForName_
+jmp   done_checking_patched
+not_modified:
+;    patched = l;
+mov   ax, si
+done_checking_patched:
+; R_InstallSpriteLump(patched, frame, rotation, false);
+;    frame = lumpinfoInit[lump].name[4] - 'A';
+;    rotation = lumpinfoInit[lump].name[5] - '0';
+mov   es, word ptr ds:[_WAD_PAGE_FRAME_PTR]
+mov   dx, word ptr es:[di + 4]
+sub   dx, 'A' + ('0' SHL 8)
+mov   bl, dh
+xor   dh, dh
+xor   bh, bh
+xor   cx, cx
+call  R_InstallSpriteLump_
+
+mov   es, word ptr ds:[_WAD_PAGE_FRAME_PTR]
+mov   dx, word ptr es:[di + 6]
+test  dl, dl
+je    skip_flip
+;	frame = lumpinfoInit[lump].name[6] - 'A';
+;	rotation = lumpinfoInit[lump].name[7] - '0';
+;	R_InstallSpriteLump(l, frame, rotation, true);
+
+sub   dx, 'A' + ('0' SHL 8)
+mov   ax, si
+mov   bl, dh
+xor   dh, dh
+xor   bh, bh
+mov   cx, 1
+call  R_InstallSpriteLump_
+
+skip_flip:
+pop   dx
+pop   cx
+
+jmp   done_processing_sprite_lump
+exit_initspritedefs:
 LEAVE_MACRO
 POPA_NO_AX_OR_BP_MACRO
 ret
