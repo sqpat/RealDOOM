93b59cd2969c029717e4e49e65f37ebd2738ff13
sqpat <sqpat17@gmail.com>
2026-04-08 12:49:14 -0700

    inline last fixedmul in span

--- r_span24.asm
+++ r_span24.asm
@@ -267,90 +267,6 @@ ENDIF
 
 
 ALIGN_MACRO	
-PROC R_FixedMulLocal24_ NEAR
-
-
-; DX:AX  *  CX:BX
-;  0  1      2  3
-
-; with sign extend for byte 3:
-; S0:DX:AX    *   S1:CX:BX
-; S0 = DX sign extend
-; S1 = CX sign extend
-
-;
-; 
-;BYTE
-; RETURN VALUE
-;                3       2       1		0
-;                DONTUSE USE     USE    DONTUSE
-
-
-;                               AXBXhi	 AXBXlo
-;                       DXBXhi  DXBXlo          
-;               S0BXhi  S0BXlo                          
-;
-;                       AXCXhi  AXCXlo
-;               DXCXhi  DXCXlo  
-;                       
-;               AXS1hi  AXS1lo
-;                               
-;                       
-;       
-
-
-
-; need to get the sign-extends for DX and CX
-
-push  si
-
-mov   es, ax	; store ax in es
-mov   ds, dx    ; store dx in ds
-mov   ax, dx	; ax holds dx
-CWD				; S0 in DX
-
-AND   DX, BX	; S0*BX
-NEG   DX
-mov   SI, DX	; DI stores hi word return
-
-; AX still stores DX
-MUL  CX         ; DX*CX
-add  SI, AX    ; low word result into high word return
-
-mov  AX, DS    ; restore DX from ds
-MUL  BX         ; DX*BX
-XCHG BX, AX    ; BX will hold low word return. store bx in ax
-add  SI, DX    ; add high word to result
-
-mov  DX, ES    ; restore AX from ES
-mul  DX        ; BX*AX  
-add  BX, DX    ; high word result into low word return
-ADC  SI, 0
-
-mov  AX, CX   ; AX holds CX
-CWD           ; S1 in DX
-
-mov  CX, ES   ; AX from ES
-AND  DX, CX   ; S1*AX
-NEG  DX
-ADD  SI, DX   ; result into high word return
-
-MUL  CX       ; AX*CX
-
-ADD  AX, BX	  ; set up final return value
-ADC  DX, SI
-
-mov  CX, SS   ; restore DS
-mov  DS, CX
-
-pop   si
-ret
-
-
-
-ENDP
-
-ALIGN_MACRO	
 go_generate_values:
 jmp   generate_distance_steps
 
@@ -472,15 +388,41 @@ public distance_steps_ready
 mov   bx, di          ; dword lookup if we add them
 
 push  dx   ; store distance high word in case needed for colormap
-les   bx, dword ptr ds:[bx + di + ((DISTSCALE_SEGMENT - SPANSTART_SEGMENT) * 16)]
+les   bp, dword ptr ds:[bx + di + ((DISTSCALE_SEGMENT - SPANSTART_SEGMENT) * 16)]
 mov   cx, es                                   	; distscale high word
 
-; todo inline this last fixedmul. and 386 version.
+; inlined R_FixedMulLocal24_
+; todo: 386 version
+; ARGS
+; DX:AX = Value1
+; CX:BP = Value2
 
-push  ds
-call R_FixedMulLocal24_
-
-pop   ds
+; PRESERVE
+; DS
+; SI
+; DI
+    MOV BX, DX
+    PUSH AX
+    MUL  BP
+    MOV  ES, DX
+    MOV  AX, BX
+    MUL  CX
+    XCHG AX, BX
+    CWD
+    AND  DX, BP
+    SUB  BX, DX
+    MUL  BP
+    MOV  BP, ES
+    ADD  AX, BP
+    ADC  BX, DX
+    XCHG AX, CX
+    CWD
+    POP BP
+    AND DX, BP
+    SUB BX, DX
+    MUL BP
+    ADD AX, CX
+    ADC DX, BX
 
 
 ;	angle = MOD_FINE_ANGLE(viewangle_shiftright3+ xtoviewangle[x1]);
