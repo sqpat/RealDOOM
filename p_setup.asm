4f6cda1ba3b023dad4d11a9c87a7c5abd828f145
sqpat <sqpat17@gmail.com>
2026-05-04 12:01:21 -0400

    store row offsets as 0-127, fixing some render bugs

--- p_setup.asm
+++ p_setup.asm
@@ -329,7 +329,9 @@ xchg   bx, di
 
 lodsw
 xchg   ax, dx ; dx stores textureoffset
-movsw   ; siderender_t rowoffset
+lodsw
+and    al, 07Fh   ; rowoffsets only make sense being 0-7F 
+stosw   ; siderender_t rowoffset ; todo store as byte
 mov    ax, word ptr ds:[si + (MAPSIDEDEF_T.mapsidedef_sector - MAPSIDEDEF_T.mapsidedef_toptexture)]  ; + 24, read ahead..
 
 SHIFT_MACRO rol ax 4   ; preserves negative 1 case
